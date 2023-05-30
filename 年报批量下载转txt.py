10c16503c11fdde9346a8723b35405eccbee41af
凌小天 <61688018+legeling@users.noreply.github.com>
2023-05-30 15:38:57 +0800

    代码更新

---
+++ 年报批量下载转txt.py
@@ -0,0 +1,127 @@
+'''
+@Project ：PycharmProjects
+@File    ：年报批量下载.py
+@IDE     ：PyCharm
+@Author  ：lingxiaotian
+@Date    ：2023/5/30 11:38
+'''
+
+import pandas as pd
+import requests
+import os
+import multiprocessing
+import pdfplumber
+import logging
+import re
+
+#日志配置文件
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+#下载模块
+def download_pdf(pdf_url, pdf_file_path):
+    try:
+        with requests.get(pdf_url, stream=True, timeout=10) as r:
+            r.raise_for_status()
+            with open(pdf_file_path, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+    except requests.exceptions.RequestException as e:
+        logging.error(f"下载PDF文件失败：{e}")
+        return False
+    else:
+        return True
+
+#文件转换
+def convert(code, name, year, pdf_url, pdf_dir, txt_dir, flag_pdf):
+    pdf_file_path = os.path.join(pdf_dir, re.sub(r'[\\/:*?"<>|]', '',f"{code:06}_{name}_{year}.pdf"))
+    txt_file_path = os.path.join(txt_dir, re.sub(r'[\\/:*?"<>|]', '', f"{code:06}_{name}_{year}.txt"))
+
+    try:
+        # 下载PDF文件
+        if not os.path.exists(pdf_file_path):
+            retry_count = 3
+            while retry_count > 0:
+                if download_pdf(pdf_url, pdf_file_path):
+                    break
+                else:
+                    retry_count -= 1
+            if retry_count == 0:
+                logging.error(f"下载失败：{pdf_url}")
+                return
+
+        # 转换PDF文件为TXT文件
+        with pdfplumber.open(pdf_file_path) as pdf:
+            with open(txt_file_path, 'w', encoding='utf-8') as f:
+                for page in pdf.pages:
+                    text = page.extract_text()
+                    f.write(text)
+
+        logging.info(f"{txt_file_path} 已保存.")
+
+    except Exception as e:
+        logging.error(f"处理 {code:06}_{name}_{year}时出错： {e}")
+    else:
+        # 删除已转换的PDF文件，以节省空间
+        if flag_pdf:
+            os.remove(pdf_file_path)
+            logging.info(f"{pdf_file_path} 已被删除.")
+
+
+
+def main(file_name,pdf_dir,txt_dir,flag_pdf):
+    print("程序开始运行，请耐心等待……")
+    # 读取Excel文件
+    try:
+        df = pd.read_excel(file_name)
+    except Exception as e:
+        logging.error(f"读取失败，请检查路径是否设置正确，建议输入绝对路径 {e}")
+        return
+    try:
+        os.makedirs(pdf_dir, exist_ok=True)
+        os.makedirs(txt_dir, exist_ok=True)
+    except Exception as e:
+        logging.error(f"创建文件夹失败！请检查文件夹是否为只读！ {e}")
+        return
+
+    # 读取文件内容并存储为字典
+    content_dict = ((row['公司代码'], row['公司简称'], row['年份'], row['年报链接']) for _, row in df.iterrows())
+
+    # 多进程下载PDF并转为TXT文件
+    with multiprocessing.Pool() as pool:
+        for code, name, year, pdf_url in content_dict:
+            txt_file_name = f"{code:06}_{name}_{year}.txt"
+            txt_file_path = os.path.join(txt_dir, txt_file_name)
+            if os.path.exists(txt_file_path):
+                logging.info(f"{txt_file_name} 已存在，跳过.")
+            else:
+                pool.apply_async(convert, args=(code, name, year, pdf_url, pdf_dir, txt_dir, flag_pdf))
+
+        pool.close()
+        pool.join()
+
+
+if __name__ == '__main__':
+    # 是否删除pdf文件，True为是，False为否
+    flag_pdf = True
+    # 是否批量处理多个年份，True为是，False为否
+    Flag = False
+    if Flag:
+        #批量下载并转换年份区间
+        for year in range(2004,2022):
+            # ===========Excel表格路径，建议使用绝对路径，务必请自行修改！！！！！！！===========
+            file_name = f"/Users/文档/MyProgram/PycharmProjects/财报数据/年报/年报链接_{year}【公众号：凌小添】.xlsx"
+            # 创建存储文件的文件夹路径，如有需要请修改
+            pdf_dir = f'年报文件/{year}/pdf年报'
+            txt_dir = f'年报文件/{year}/txt年报'
+            main(file_name,pdf_dir,txt_dir,flag_pdf)
+            print(f"{year}年年报处理完毕，若报错，请检查后重新运行")
+    else:
+        #处理单独年份：
+        #特定年份的excel表格，请务必修改。
+        year = 2022
+        file_name = f"/Users/wangjialong/文档/MyProgram/PycharmProjects/Spider/财报数据/年报/年报链接_{year}【公众号：凌小添】.xlsx"
+        pdf_dir = f'年报文件/{year}/pdf年报'
+        txt_dir = f'年报文件/{year}/txt年报'
+        main(file_name, pdf_dir, txt_dir, flag_pdf)
+        print(f"{year}年年报处理完毕，若报错，请检查后重新运行")
--- 词频分析.py
+++ 词频分析.py
@@ -1,7 +1,15 @@
+'''
+@Project ：PycharmProjects
+@File    ：词频分析.py
+@IDE     ：PyCharm
+@Author  ：lingxiaotian
+@Date    ：2023/5/30 14:34
+'''
 import os
 import re
 import xlwt
 import jieba
+
 
 def extract_keywords(filename, keywords):
     """
@@ -25,66 +33,160 @@ def extract_keywords(filename, keywords):
 
     return keyword_counts
 
-def process_files(folder_path, keywords):
+
+def count_total_words(filename):
+    """
+    统计文本的总字数。
+    """
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            content = f.read()
+        total_words = len(content)
+    except Exception as e:
+        print(f"统计文本总字数失败！--- {filename}")
+        print(str(e))
+        total_words = 0
+
+    return total_words
+
+
+def process_files(folder_path, keywords, result_name, flag, enable_word_count):
     """
     处理指定文件夹中的所有txt文件，提取关键词并统计词频，将结果存储到Excel表格中。
     """
     # 创建Excel工作簿
     workbook = xlwt.Workbook(encoding='utf-8')
-    worksheet = workbook.add_sheet('Sheet1')
+    worksheet = workbook.add_sheet('公众号凌小添')
     row = 0
     # 添加Excel表头
     worksheet.write(row, 0, '股票代码')
     worksheet.write(row, 1, '公司简称')
     worksheet.write(row, 2, '年份')
+    if enable_word_count:
+        worksheet.write(row, 3, '文本总字数')
+        col_offset = 4
+    else:
+        col_offset = 3
     for i, keyword in enumerate(keywords):
-        worksheet.write(row, i + 3, keyword)
+        worksheet.write(row, i + col_offset, keyword)
     row += 1
 
-    # 获取文件总数
-    total_files = len([filename for filename in os.listdir(folder_path) if filename.endswith('.txt')])
-    processed_files = 0
+    # 遍历文件夹中的所有文件
+    for root, dirs, files in os.walk(folder_path):
+        for filename in files:
+            if filename.endswith('.txt'):
+                # 解析文件名，提取股票代码、公司简称和年份
+                match = re.match(r'^(\d{6})_(.*?)_(\d{4})\.txt$', filename)
+                if match:
+                    stock_code = match.group(1)
+                    company_name = match.group(2)
+                    year = match.group(3)
+                    # 处理年份筛选
+                    if flag and year not in flag:
+                        continue
 
-    # 遍历文件夹中的所有txt文件
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.txt'):
-            # 解析文件名，提取股票代码、公司简称和年份
-            match = re.match(r'^(\d{6})_(.*?)_(\d{4})\.txt$', filename)
-            if match:
-                stock_code = match.group(1)
-                company_name = match.group(2)
-                year = match.group(3)
+                    file_path = os.path.join(root, filename)
 
-                # 提取关键词并统计词频
-                keyword_counts = extract_keywords(os.path.join(folder_path, filename), keywords)
+                    # 提取关键词并统计词频
+                    keyword_counts = extract_keywords(file_path, keywords)
 
-                # 将结果写入Excel表格
-                worksheet.write(row, 0, stock_code)
-                worksheet.write(row, 1, company_name)
-                worksheet.write(row, 2, year)
-                for i, count in enumerate(keyword_counts):
-                    worksheet.write(row, i + 3, count)
-                row += 1
+                    # 统计文本总字数
+                    total_words = count_total_words(file_path)
 
-                # 更新进度
-                processed_files += 1
-                progress = (processed_files / total_files) * 100
-                print(f"\r当前进度: {progress:.2f}%", end='', flush=True)
+                    # 将结果写入Excel表格
+                    worksheet.write(row, 0, stock_code)
+                    worksheet.write(row, 1, company_name)
+                    worksheet.write(row, 2, year)
+                    if enable_word_count:
+                        worksheet.write(row, 3, total_words)
+                        col_offset = 4
+                    else:
+                        col_offset = 3
+                    for i, count in enumerate(keyword_counts):
+                        worksheet.write(row, i + col_offset, count)
+                    row += 1
+                else:
+                    # 尝试第二种文件名格式：600519-贵州茅台2016年年度报告.txt
+                    match = re.match(r'(\d{6})-(.*?)((19|20)\d{2})', filename)
+                    if match:
+                        stock_code = match.group(1)
+                        company_name = match.group(2)
+                        year = match.group(3)
+                        # 处理年份筛选
+                        if flag and year not in flag:
+                            continue
+
+                        file_path = os.path.join(root, filename)
+
+                        # 提取关键词并统计词频
+                        keyword_counts = extract_keywords(file_path, keywords)
+
+                        # 统计文本总字数
+                        total_words = count_total_words(file_path)
+
+                        # 将结果写入Excel表格
+                        worksheet.write(row, 0, stock_code)
+                        worksheet.write(row, 1, company_name)
+                        worksheet.write(row, 2, year)
+                        if enable_word_count:
+                            worksheet.write(row, 3, total_words)
+                            col_offset = 4
+                        else:
+                            col_offset = 3
+                        for i, count in enumerate(keyword_counts):
+                            worksheet.write(row, i + col_offset, count)
+                        row += 1
+                    else:
+                        # 无法解析文件名，跳过当前文件
+                        print(f"无法解析文件名：{filename}")
+                        continue
 
     # 保存Excel文件
     try:
-        workbook.save('result20200.xls')
+        workbook.save(result_name)
     except Exception as e:
-        print("Failed to save Excel file.")
+        print("保存到Excel失败")
         print(str(e))
 
-if __name__ == '__main__':
-    # 设置要提取的关键词列表
-    keywords = ['财务', '共享服务', '财务共享','利润','数字化','培训','云计算','大数据','互联网']
 
+if __name__ == '__main__':
+    # 设置要提取的关键词列表，此处请自行修改！！！！
+    # keywords = ['人工智能', '物联网', '财务共享', '利润', '数字化',  '云计算', '大数据', '互联网']
+    keywords= [
+        '人工智能', '商业智能', '图像理解',
+        '投资决策辅助系统', '智能数据分', '大数据', '数据挖掘', '文本挖掘',
+        '智能机器人', '机器学习', '深度学习','数据可视化', '异构数据',
+        '语义搜索', '生物识别技术', '混合现实', '虚拟现实',
+        '人脸识别', '语音识别', '身份验证', '自动驾驶', '自然语言处理',
+        '企业数字化转型','云计算', '流计算',
+        '图计算', '内存计算', '多方安全计算', '类脑计算', '綠色计算',
+        '认知计算', '融合架构', '亿级并发', '区块链', '数字货币',
+        '分布式计算', 'EB级存储', '物联网', '信息',
+        '差分隐私技术', '智能金融合约', '物理系统', '移动互联网',
+        '工业互联网', '移动互联', '互联网医疗', '电子商务', '移动支付',
+        '第三方支付', 'NFC支付', '智能能源', 'B2B', 'B2C',
+        'C2B', 'C2C', '020', '网联', '智能穿戴',
+        '智慧农业', '智能交通', '智能医疗', '智能客服', '智能家居',
+        '智能投顾', '智能文旅', '智能环保', '智能电网', '智能营销',
+        '数字营销', '无人零售', '互联网金融', '数学金融', 'Fintech',
+        '金融科技', '量化金融', '开放银行'
+        ]
+
+    # 要分析的年报所在的文件夹，尽量使用绝对路径，可以是多级目录
+    folder_path = "/Users/文档/MyProgram/PycharmProjects/财报数据/年报文件"
+    result_name = "词频分析结果【公众号凌小添】.xlsx"
+    # 是否筛选特定年份，为列表模式，设置为 None 则不筛选
+    # flag = None
+    flag = ['2020','2021','2022']
+
+    # 是否开启统计文本总字数
+    enable_word_count = True
     # 处理文件夹中的所有txt文件，并将结果存储到Excel表格中
     try:
-        process_files('txt年报2020', keywords)
+        process_files(folder_path, keywords, result_name, flag, enable_word_count)
     except Exception as e:
         print("文件处理失败！！")
         print(str(e))
+    #！！！注意：如果程序运行无反应，多半是路径和txt文件命名问题！
+    # 推荐文件名命名格式：“600519_贵州茅台_2019.txt”
+    # 本代码亦支持如下格式：“600519-贵州茅台2019年年度报告.txt”
